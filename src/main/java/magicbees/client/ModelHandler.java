cf614ea6f6e21df147fadfe90a1d19ec6680c410
a <EYCHFSJD@gmail.com>
2024-03-15 00:45:39 +0800

    thaumcraft integration

--- src/main/java/magicbees/client/ModelHandler.java
+++ src/main/java/magicbees/client/ModelHandler.java
@@ -115,8 +115,8 @@ public class ModelHandler {
         setItemModelLocation(manasteelgrafter, createMRL("manasteel_grafter"));
         setItemModelLocation(manasteelScoop, createMRL("manasteel_scoop"));
 
-        setItemModelLocation(manasteelgrafter, createMRL("thaumium_grafter"));
-        setItemModelLocation(manasteelScoop, createMRL("thaumium_scoop"));
+        setItemModelLocation(thaumiumgrafter, createMRL("thaumium_grafter"));
+        setItemModelLocation(thaumiumScoop, createMRL("thaumium_scoop"));
 
         setItemModelLocation(jellyBaby, createMRL("jelly_babies"));
 
